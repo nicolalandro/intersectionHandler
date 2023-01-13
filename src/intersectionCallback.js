398b5bb8bdccb65cab1301288c9d27ca2faf1560
zp <zeropara@gmail.com>
2023-01-13 17:07:55 +0100

    rimosso passaggio observer inutile

--- src/intersectionCallback.js
+++ src/intersectionCallback.js
@@ -1,14 +1,14 @@
 import callbacksRegistry from './callbacksRegistry'
 
 /**
- *
+ * Callback called each time the observer notifies do so
  * @param {Array} entries
  */
-const intersectionCallback = (entries, observer) => {
+const intersectionCallback = (entries) => {
   entries.forEach(async (entry) => {
     const { target } = entry
     const entryCallback = callbacksRegistry.get(target)
-    entryCallback(entry, observer)
+    entryCallback(entry)
   })
 }
 
