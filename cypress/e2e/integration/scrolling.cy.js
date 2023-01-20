e88f2f4540046447aa121764cfc1626a4b3dcdcf
zp <zeropara@gmail.com>
2023-01-20 18:09:14 +0100

    removed unnecesssary nesting

--- cypress/e2e/integration/scrolling.cy.js
+++ cypress/e2e/integration/scrolling.cy.js
@@ -8,46 +8,44 @@ describe('checking scroll behaviour', () => {
   it("scrolls down the page and checks if callbacks had proper execution", () => {
     cy.visit(baseURL)
    
-    cy.window().then((win) => {
-      cy.wait(150)
-      cy.contains('Welcome').parent().parent().should('have.class', 'in-view')
-      cy.contains('To').parent().parent().should('have.class', 'in-view')
+    
+    cy.contains('Welcome').parent().parent().should('have.class', 'in-view')
+    cy.contains('To').parent().parent().should('have.class', 'in-view')
 
-      cy.contains('TESTING').should('not.exist')
-      cy.contains('Micro').should('not.exist')
-      cy.contains('Example').should('not.exist')
-      console.log( cy.get('.fullviewport').eq(0))
-      cy.document().then((doc) => {
-       
-        const els = Array.from(doc.querySelectorAll('.fullviewport'))
-        els[1].scrollIntoView()
-        
-      });
-
-      cy.contains('TESTING').parent().should('have.class', 'fullviewport')
-      cy.contains('TESTING').parent().should('have.class', 'in-view')
-
-
-      cy.wait(150)
-      cy.document().then((doc) => {
-       
-        const els = Array.from(doc.querySelectorAll('.fullviewport'))
-        els[2].scrollIntoView()
-        
-      });
+    cy.contains('TESTING').should('not.exist')
+    cy.contains('Micro').should('not.exist')
+    cy.contains('Example').should('not.exist')
+    console.log( cy.get('.fullviewport').eq(0))
+    cy.document().then((doc) => {
       
-      cy.contains('Micro').parent().parent().should('have.class', 'in-view')
-      cy.contains('Example').parent().parent().should('have.class', 'in-view')
-      cy.document().then((doc) => {
-       
-        const els = Array.from(doc.querySelectorAll('.fullviewport'))
-        els[3].scrollIntoView()
-        
-      });
+      const els = Array.from(doc.querySelectorAll('.fullviewport'))
+      els[1].scrollIntoView()
+      
+    });
+
+    cy.contains('TESTING').parent().should('have.class', 'fullviewport')
+    cy.contains('TESTING').parent().should('have.class', 'in-view')
+
+
+    
+    cy.document().then((doc) => {
+      
+      const els = Array.from(doc.querySelectorAll('.fullviewport'))
+      els[2].scrollIntoView()
+      
+    });
+    
+    cy.contains('Micro').parent().parent().should('have.class', 'in-view')
+    cy.contains('Example').parent().parent().should('have.class', 'in-view')
+    cy.document().then((doc) => {
+      
+      const els = Array.from(doc.querySelectorAll('.fullviewport'))
+      els[3].scrollIntoView()
+      
+    });
       
      
-      cy.contains('TESTING').should('not.exist') 
-    })
+    cy.contains('TESTING').should('not.exist')
   })
 })
 
